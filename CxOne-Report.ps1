3220d2904fed2cc11f3d65a2dbece27b09b4deec
Michael Fowler <101998836+cx-michael-fowler@users.noreply.github.com>
2026-07-24 15:20:47 +1000

    v2.0

--- CxOne-Report.ps1
+++ CxOne-Report.ps1
@@ -21,8 +21,8 @@ Report
     
 
 .Notes
-Version:     1.1
-Date:        05/03/2026
+Version:     2.0
+Date:        24/07/2026
 Written by:  Michael Fowler
 Contact:     michael.fowler@checkmarx.com
 
@@ -31,6 +31,7 @@ Version    Detail
 -----------------
 1.0        Original version
 1.1        Updated to rename Secrets to SSCS
+2.0        Added languages and state data
 
   
 .PARAMETER help
@@ -104,7 +105,7 @@ Begin {
     #region Functions
 
     # Get Dates, Contributer counts and Versions
-    Function Get-GeneralData {
+    Function Export-GeneralData {
         param (
             [Parameter(Mandatory=$true)][CxOneConnection]$conn,
             [Parameter(Mandatory=$true)][AllowEmptyCollection()][System.Collections.ArrayList]$files
@@ -127,7 +128,7 @@ Begin {
     }
 
     #Get Application data
-    Function Get-Apps {
+    Function Export-Apps {
         param (
             [Parameter(Mandatory=$true)][CxOneConnection]$conn,
             [Parameter(Mandatory=$true)][System.Collections.ArrayList]$files
@@ -141,7 +142,7 @@ Begin {
     }
 
     #Get project data
-    Function Get-Projects {
+    Function Export-Projects {
         param (
             [Parameter(Mandatory=$true)][CxOneConnection]$conn,
             [Parameter(Mandatory=$true)][System.Collections.ArrayList]$files
@@ -155,7 +156,7 @@ Begin {
     }
 
     #Get Scans data. Return scans to retrieve severity counters
-    Function Get-Scans {
+    Function Export-Scans {
         param (
             [Parameter(Mandatory=$true)][CxOneConnection]$conn,
             [Parameter(Mandatory=$true)][int]$scanDays,
@@ -164,9 +165,12 @@ Begin {
 
         #Scans
         $outputFile = Join-Path -Path "$env:TEMP" -ChildPath "scans.csv"
-        $scans  = Get-ScansByDays $conn Completed,Failed,Partial $scanDays
+        $scans  = Get-ScansByDays $conn Completed,Failed,Partial $scanDays -getLanguages
         $scans.Values |
-            Select-Object ScanID,ProjectId,ProjectName,Status,Branch,Loc,CreatedAt,StartDate,EndDate,Runtime,@{N=’Engines’; E={$_.EnginesString}},Initiator,SourceType,SourceOrigin | 
+            Select-Object ScanID,ProjectId,ProjectName,Status,Branch,Loc,CreatedAt,StartDate,EndDate,Runtime,
+                          @{N=’Engines’;E={$_.EnginesString}},Initiator,SourceType,SourceOrigin,
+                          @{N=’ScannedLanguages’;E={$_.SastScanMetrics.ScannedLanguagesString}},
+                          @{N=’NotScannedLanguages’; E={$_.SastScanMetrics.DetectedButNotScannedLanguagesString}} | 
             export-csv $outputFile -NoTypeInformation
         $files.Add($outputFile) | out-null
 
@@ -191,73 +195,154 @@ Begin {
         return $scans
     }
 
-    #Get Severity Counters
-    Function Get-Severities {
+    #Export Scan Summaries
+    Function Export-ScanSummaries {
         param (
             [Parameter(Mandatory=$true)][CxOneConnection]$conn,
             [Parameter(Mandatory=$true)][System.Collections.Generic.Dictionary[String, Scan]]$scans,
             [Parameter(Mandatory=$true)][System.Collections.ArrayList]$files
         )
 
-        $outputFile = Join-Path -Path "$env:TEMP" -ChildPath "counts.csv"
-        $counts = Get-SeverityCounters $conn $scans
-        $counts.GetEnumerator() | ForEach-Object {
+        $summary = Get-ScanSummaries $conn $scans
+        Export-Severities $summary $files
+        Export-States $summary $files
+    }
+
+    #Export Severities
+    Function Export-Severities {
+        param (
+            [Parameter(Mandatory=$true)][AllowEmptyCollection()][System.Collections.Generic.Dictionary[String, ScanSummary]]$summary,
+            [Parameter(Mandatory=$true)][System.Collections.ArrayList]$files
+        )      
+        
+        $outputFile = Join-Path -Path "$env:TEMP" -ChildPath "severityCounts.csv"
+        $summary.GetEnumerator() | ForEach-Object {
             [PSCustomObject]@{
                 ScanID = $_.Key
-                Total_Vulnerabilties = $_.Value.Totals.Total
-                Total_Critical = $_.Value.Totals.Critical
-                Total_High = $_.Value.Totals.High
-                Total_Medium = $_.Value.Totals.Medium
-                Total_Low = $_.Value.Totals.Low
-                Total_Info = $_.Value.Totals.Info
-                Sast_Total_Vulnerabilties = $_.Value.Sast.Total
-                Sast_Critical = $_.Value.Sast.Critical
-                Sast_High = $_.Value.Sast.High
-                Sast_Medium = $_.Value.Sast.Medium
-                Sast_Low = $_.Value.Sast.Low
-                Sast_Info = $_.Value.Sast.Info
-                Kics_Total_Vulnerabilties = $_.Value.Kics.Total
-                Kics_Critical = $_.Value.Kics.Critical
-                Kics_High = $_.Value.Kics.High
-                Kics_Medium = $_.Value.Kics.Medium
-                Kics_Low = $_.Value.Kics.Low
-                Kics_Info = $_.Value.Kics.Info
-                Sca_Total_Vulnerabilties = $_.Value.Sca.Total
-                Sca_Critical = $_.Value.Sca.Critical
-                Sca_High = $_.Value.Sca.High
-                Sca_Medium = $_.Value.Sca.Medium
-                Sca_Low = $_.Value.Sca.Low
-                Sca_Info = $_.Value.Sca.Info
-                Packages_Total_Vulnerabilties = $_.Value.Packages.Total
-                Packages_Critical = $_.Value.Packages.Critical
-                Packages_High = $_.Value.Packages.High
-                Packages_Medium = $_.Value.Packages.Medium
-                Packages_Low = $_.Value.Packages.Low
-                Packages_Info = $_.Value.Packages.Info
-                Api_Total_Vulnerabilties = $_.Value.Api.Total
-                Api_Critical = $_.Value.Api.Critical
-                Api_High = $_.Value.Api.High
-                Api_Medium = $_.Value.Api.Medium
-                Api_Low = $_.Value.Api.Low
-                Api_Info = $_.Value.Api.Info
-                SSCS_Total_Vulnerabilties = $_.Value.SSCS.Total
-                SSCS_Critical = $_.Value.SSCS.Critical
-                SSCS_High = $_.Value.SSCS.High
-                SSCS_Medium = $_.Value.SSCS.Medium
-                SSCS_Low = $_.Value.SSCS.Low
-                SSCS_Info = $_.Value.SSCS.Info
-                Containers_Total_Vulnerabilties = $_.Value.Containers.Total
-                Containers_Critical = $_.Value.Containers.Critical
-                Containers_High = $_.Value.Containers.High
-                Containers_Medium = $_.Value.Containers.Medium
-                Containers_Low = $_.Value.Containers.Low
-                Containers_Info = $_.Value.Containers.Info
+                Total_Vulnerabilties = $_.Value.Totals.Severities.Total
+                Total_Critical = $_.Value.Totals.Severities.Critical
+                Total_High = $_.Value.Totals.Severities.High
+                Total_Medium = $_.Value.Totals.Severities.Medium
+                Total_Low = $_.Value.Totals.Severities.Low
+                Total_Info = $_.Value.Totals.Severities.Info
+                
+                Sast_Total_Vulnerabilties = $_.Value.Sast.Severities.Total
+                Sast_Critical = $_.Value.Sast.Severities.Critical
+                Sast_High = $_.Value.Sast.Severities.High
+                Sast_Medium = $_.Value.Sast.Severities.Medium
+                Sast_Low = $_.Value.Sast.Severities.Low
+                Sast_Info = $_.Value.Sast.Severities.Info
+                
+                Kics_Total_Vulnerabilties = $_.Value.Kics.Severities.Total
+                Kics_Critical = $_.Value.Kics.Severities.Critical
+                Kics_High = $_.Value.Kics.Severities.High
+                Kics_Medium = $_.Value.Kics.Severities.Medium
+                Kics_Low = $_.Value.Kics.Severities.Low
+                Kics_Info = $_.Value.Kics.Severities.Info
+
+                Sca_Total_Vulnerabilties = $_.Value.Sca.Severities.Total
+                Sca_Critical = $_.Value.Sca.Severities.Critical
+                Sca_High = $_.Value.Sca.Severities.High
+                Sca_Medium = $_.Value.Sca.Severities.Medium
+                Sca_Low = $_.Value.Sca.Severities.Low
+                Sca_Info = $_.Value.Sca.Severities.Info
+
+                Packages_Total_Vulnerabilties = $_.Value.Packages.Severities.Total
+                Packages_Critical = $_.Value.Packages.Severities.Critical
+                Packages_High = $_.Value.Packages.Severities.High
+                Packages_Medium = $_.Value.Packages.Severities.Medium
+                Packages_Low = $_.Value.Packages.Severities.Low
+                Packages_Info = $_.Value.Packages.Severities.Info
+
+                Api_Total_Vulnerabilties = $_.Value.Api.Severities.Total
+                Api_Critical = $_.Value.Api.Severities.Critical
+                Api_High = $_.Value.Api.Severities.High
+                Api_Medium = $_.Value.Api.Severities.Medium
+                Api_Low = $_.Value.Api.Severities.Low
+                Api_Info = $_.Value.Api.Severities.Info
+
+                SSCS_Total_Vulnerabilties = $_.Value.SSCS.Severities.Total
+                SSCS_Critical = $_.Value.SSCS.Severities.Critical
+                SSCS_High = $_.Value.SSCS.Severities.High
+                SSCS_Medium = $_.Value.SSCS.Severities.Medium
+                SSCS_Low = $_.Value.SSCS.Severities.Low
+                SSCS_Info = $_.Value.SSCS.Severities.Info
+
+                Containers_Total_Vulnerabilties = $_.Value.Containers.Severities.Total
+                Containers_Critical = $_.Value.Containers.Severities.Critical
+                Containers_High = $_.Value.Containers.Severities.High
+                Containers_Medium = $_.Value.Containers.Severities.Medium
+                Containers_Low = $_.Value.Containers.Severities.Low
+                Containers_Info = $_.Value.Containers.Severities.Info
             }
         } | export-csv $outputFile -NoTypeInformation
         $files.Add($outputFile) | out-null
-
     }
 
+    #Export States
+    Function Export-States {
+        param (
+            [Parameter(Mandatory=$true)][AllowEmptyCollection()][System.Collections.Generic.Dictionary[String, ScanSummary]]$summary,
+            [Parameter(Mandatory=$true)][System.Collections.ArrayList]$files
+        ) 
+        
+        $outputFile = Join-Path -Path "$env:TEMP" -ChildPath "stateCounts.csv"
+        $summary.GetEnumerator() | ForEach-Object {
+            [PSCustomObject]@{
+                ScanID = $_.Key
+                Total_Confirmed = $_.Value.Totals.States.Confirmed
+                Total_ToVerify = $_.Value.Totals.States.ToVerify
+                Total_Urgent = $_.Value.Totals.States.Urgent
+                Total_NotExploitable = $_.Value.Totals.States.NotExploitable
+                Total_ProposedNotExploitable = $_.Value.Totals.States.ProposedNotExploitable
+
+                Sast_Confirmed  = $_.Value.Sast.States.Confirmed
+                Sast_ToVerify = $_.Value.Sast.States.ToVerify
+                Sast_Medium = $_.Value.Sast.States.Urgent
+                Sast_NotExploitable = $_.Value.Sast.States.NotExploitable
+                Sast_ProposedNotExploitable = $_.Value.Sast.States.ProposedNotExploitable
+
+                Kics_Confirmed  = $_.Value.Kics.States.Confirmed
+                Kics_ToVerify = $_.Value.Kics.States.ToVerify
+                Kics_Urgent = $_.Value.Kics.States.Urgent
+                Kics_NotExploitable = $_.Value.Kics.States.NotExploitable
+                Kics_ProposedNotExploitable = $_.Value.Kics.States.ProposedNotExploitable
+
+                Sca_Confirmed  = $_.Value.Sca.States.Confirmed
+                Sca_ToVerify = $_.Value.Sca.States.ToVerify
+                Sca_Urgent = $_.Value.Sca.States.Urgent
+                Sca_NotExploitable = $_.Value.Sca.States.NotExploitable
+                Sca_ProposedNotExploitable = $_.Value.Sca.States.ProposedNotExploitable
+
+                Packages_Confirmed  = $_.Value.Packages.States.Confirmed
+                Packages_ToVerify = $_.Value.Packages.States.ToVerify
+                Packages_Urgent = $_.Value.Packages.States.Urgent
+                Packages_NotExploitable = $_.Value.Packages.States.NotExploitable
+                Packages_ProposedNotExploitable = $_.Value.Packages.States.ProposedNotExploitable
+
+                Api_Confirmed  = $_.Value.Api.States.Confirmed
+                Api_ToVerify = $_.Value.Api.States.ToVerify
+                Api_Urgent = $_.Value.Api.States.Medium
+                Api_NotExploitable = $_.Value.Api.States.NotExploitable
+                Api_ProposedNotExploitable = $_.Value.Api.States.ProposedNotExploitable
+
+                SSCS_Confirmed  = $_.Value.SSCS.States.Confirmed
+                SSCS_ToVerify = $_.Value.SSCS.States.High
+                SSCS_Urgent = $_.Value.SSCS.States.Urgent
+                SSCS_NotExploitable = $_.Value.SSCS.States.NotExploitable
+                SSCS_ProposedNotExploitable = $_.Value.SSCS.States.ProposedNotExploitable
+
+                Containers_Confirmed  = $_.Value.Containers.States.Confirmed
+                Containers_ToVerify = $_.Value.Containers.States.ToVerify
+                Containers_Urgent = $_.Value.Containers.States.Urgent
+                Containers_NotExploitable = $_.Value.Containers.States.NotExploitable
+                Containers_ProposedNotExploitable = $_.Value.Containers.States.ProposedNotExploitable
+            }
+        } | export-csv $outputFile -NoTypeInformation
+        $files.Add($outputFile) | out-null        
+    }
+
+ 
 
     #endregion
     #----------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -290,28 +375,28 @@ Process {
 
      #General Data
     Write-Host "Retrieving General Information"
-    Get-GeneralData $conn $files
+    Export-GeneralData $conn $files
     Write-Host "General details written to file"
 
     #Applications
     Write-Host "Retrieving Applications"
-    Get-Apps $conn $files
+    Export-Apps $conn $files
     Write-Host "Applications written to file"
 
     #Projects
     Write-Host "Retrieving Projects"
-    Get-Projects $conn $files
+    Export-Projects $conn $files
     Write-Host "Projects written to file"
 
     #Scans and Statuses
     Write-Host "Retrieving Scans"
-    $scans = Get-Scans $conn $scanDays $files
+    $scans = Export-Scans $conn $scanDays $files
     Write-Host "Scans written to file"
 
-    #Severity Counters
-    Write-Host "Retrieving Severity Counters"
-    Get-Severities $conn $scans $files
-    Write-Host "Severity counts written to file"
+    #Severity and States Counters
+    Write-Host "Retrieving Scan Summeries"
+    Export-ScanSummaries $conn $scans $files
+    Write-Host "Scan Summeries written to file"
 
     #Zip files and save to script location
     Compress-Archive -Path $files -DestinationPath "$PSScriptRoot\data.zip" -Force
